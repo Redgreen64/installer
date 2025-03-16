d9be6ac76142e764d59ee7dcc81238ea65b9ea14
Redgreen64 <koltsc2014+@gmail.com>
2025-03-16 14:57:18 +0000

    Create main_nosound.py

---
+++ installer/folder/main_nosound.py
@@ -0,0 +1,36 @@
+
+import google.generativeai as genai
+from time import sleep
+genai.configure(api_key="YOUR_API_KEY")
+  
+os.system('clear')
+print("O_O")
+
+generation_config = {
+  "temperature": 2,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(model_name="gemini-2.0-flash",
+                              system_instruction=[
+        "",
+    ], generation_config=generation_config,)
+def talk():
+    userinput = input()
+    os.system('clear')
+    print("/o~o")
+    response = model.generate_content(f"{userinput}")
+    os.system('clear')
+    print("\OoO/")
+    print(response.text)
+    sleep(4)
+    os.system('clear')
+    print("O_O")
+try:
+    while True:
+        talk()
+except KeyboardInterrupt:
+    print(f"closed {f.name}!")
