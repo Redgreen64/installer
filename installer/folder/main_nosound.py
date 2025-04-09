a123a85b23fe3ec9a29c18313c03385affcb897c
Redgreen64 <koltsc2014+@gmail.com>
2025-04-09 07:53:09 +0100

    Update main_nosound.py

--- installer/folder/main_nosound.py
+++ installer/folder/main_nosound.py
@@ -5,7 +5,8 @@ from time import sleep
 genai.configure(api_key="YOUR_API_KEY")
   
 os.system('clear')
-print("O_O")
+print("O O")
+print(" V ")
 
 generation_config = {
   "temperature": 2,
@@ -19,11 +20,12 @@ model = genai.GenerativeModel(model_name="gemini-2.0-flash",
                               system_instruction=[
         "",
     ], generation_config=generation_config,)
+chat = model.start_chat(history=[])
 def talk():
     userinput = input()
     os.system('clear')
     print("/o~o")
-    response = model.generate_content(f"{userinput}")
+    response = chat.send_message(f"{userinput}")
     os.system('clear')
     print("\OoO/")
     print(response.text)
