e04d285e1f079a4d42b7665fe514db97859a1a2c
Redgreen64 <koltsc2014+@gmail.com>
2026-02-14 00:01:33 +0000

    Refactor output clearing using sys.stdout.flush()
    Replaced os.system('clear') calls with sys.stdout.flush() for better output handling. Also replaced the thinking emoticon

--- installer/folder/main.py
+++ installer/folder/main.py
@@ -1,13 +1,14 @@
 
 import google.generativeai as genai
 import os
+import sys
 from pygame import mixer
 from time import sleep
 genai.configure(api_key="YOUR_API_KEY")
 loop = 1
 mixer.init()
   
-os.system('clear')
+sys.stdout.flush()
 print("O_O")
 
 generation_config = {
@@ -26,13 +27,13 @@ chat = model.start_chat(history=[])
 
 def talk():
     userinput = input()
-    os.system('clear')
-    print("/o~o")
+    sys.stdout.flush()
+    print("/-_-\")
     response = chat.send_message(f"{userinput}")
-    os.system('clear')
+    sys.stdout.flush()
     print("\OoO/")
     os.system(f'espeak-ng "{response.text}"')
-    os.system('clear')
+    sys.stdout.flush()
     print("O_O")
 try:
     while True:
